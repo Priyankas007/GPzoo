447a6c232553f90e8716e6d04621610e2f4f9f34
Priyanka Shrestha <priyankaspace@gmail.com>
2023-10-17 17:37:19 -0700

    added vnngp

--- gpzoo/gp.py
+++ gpzoo/gp.py
@@ -6,6 +6,84 @@ import tqdm
 from .utilities import add_jitter
 
 
+class GaussianLikelihood(nn.Module):
+  def __init__(self, gp):
+    super().__init__()
+    self.gp = gp
+
+    self.noise = nn.Parameter(torch.tensor(0.1))
+
+  def forward(self, X, E=1, verbose=False):
+    qF, qU, pU = self.gp(X, verbose=verbose)
+    F = qF.rsample((E, ))
+    pY = distributions.Normal(F, torch.abs(self.noise))
+
+    return pY, qF, qU, pU
+
+class VNNGP(nn.Module):
+  def __init__(self, kernel, dim=1, M=50, K=3, jitter=1e-4):
+    super().__init__()
+    self.kernel = kernel
+    self.jitter = jitter
+    
+    self.K = K
+    self.Z = nn.Parameter(torch.randn((M, dim))) #choose inducing points
+    self.Lu = nn.Parameter(torch.randn((M, M)))
+    self.mu = nn.Parameter(torch.zeros((M,)))
+    self.constraint = constraints.lower_cholesky
+
+  def forward(self, X, verbose=False):
+    if verbose:
+      print('calculating Kxx')
+    Kxx = self.kernel(X, X, diag=True)
+
+    if verbose:
+      print('calculating Kzx')
+    Kzx, distances = self.kernel(self.Z, X, return_distance=True)
+
+    if verbose:
+      print('calculating kzz')
+    Kzz = self.kernel(self.Z, self.Z)
+    Lu = transform_to(self.constraint)(self.Lu)
+
+    L = torch.linalg.cholesky(add_jitter(Kzz, self.jitter))
+
+    indexes = torch.argsort(distances.T, dim=1)[:, :self.K]
+
+    N = len(Kxx)
+
+
+    mean = torch.zeros_like(Kxx)
+    cov = torch.zeros_like(Kxx)
+
+    for i in range(N):
+
+      little_Kzz = (Kzz[indexes[i]])[:, indexes[i]]
+      kzz_inv = torch.inverse(add_jitter(little_Kzz, self.jitter))
+
+      W = kzz_inv @ ((Kzx[indexes[i], i]).unsqueeze(-1))
+
+      W = torch.transpose(W, -2, -1) # 1x3 matrix, Kxz@(Kzz)-1
+
+      if verbose:
+        print('W.shape:', W.shape)
+
+      little_mu = self.mu[indexes[i]]
+
+      mean[i] = torch.squeeze(W @  little_mu.unsqueeze(-1))
+
+      little_Lu = Lu[indexes[i]]
+      S = little_Lu @ torch.transpose(little_Lu, -2, -1)
+      cov[i] = Kxx[i] - torch.squeeze(W @ (little_Kzz-S) @ torch.transpose(W, -2, -1))
+
+    qF = distributions.Normal(mean, torch.clamp(cov, min=5e-2) ** 0.5)
+    qU = distributions.MultivariateNormal(self.mu, scale_tril=Lu)
+    pU = distributions.MultivariateNormal(torch.zeros_like(self.mu), scale_tril=L)
+
+    return qF, qU, pU
+
+
+
 class SVGP(nn.Module):
   def __init__(self, kernel, dim=1, M=50, jitter=1e-4):
     super().__init__()
